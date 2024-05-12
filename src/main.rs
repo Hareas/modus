7ffabd927f6497c4e24ae2d88ea0873b189e5efa
didntask <dontcare>
2024-05-12 16:56:03 +0100

    Added structuctures to hold the information and basic code to deserialize the data in the body

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use time::macros::datetime;
 use modus::yahoo_finance::{get_quotes};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Portfolio {
+    portfolio: Vec<Equity>
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Equity {
+    ticker: String,
+    buy_date: String,
+    sell_date: Option<String>,
+    buy_price: u64,
+    sell_price: Option<u64>,
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -16,7 +31,8 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
-async fn index() -> impl Responder {
+async fn index(item: web::Json<Portfolio>) -> impl Responder {
+    println!("model: {:?}", &item);
     let start = datetime!(2020-1-1 0:00:00.00 UTC);
     let end = datetime!(2020-1-31 23:59:59.99 UTC);
     get_quotes("AAPL", &start, &end).await
