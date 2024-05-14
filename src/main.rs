014f99eecf983c70ce8e21d77da405fa9b225871
didntask <dontcare>
2024-05-14 23:07:15 +0100

    Modified the structure to hold multiple positions and iterate through them to get the quotes, also modified the petition to send the appropiate dates

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use time::macros::datetime;
+use time::macros::{time};
 use modus::yahoo_finance::{get_quotes};
 use serde::{Deserialize, Serialize};
+use time::{OffsetDateTime, Month};
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Portfolio {
@@ -11,12 +12,39 @@ struct Portfolio {
 #[derive(Debug, Serialize, Deserialize)]
 struct Equity {
     ticker: String,
-    buy_date: String,
-    sell_date: Option<String>,
-    buy_price: u64,
-    sell_price: Option<u64>,
+    buy_date: TransactionDate,
+    sell_date: Option<TransactionDate>,
+    buy_price: f64,
+    sell_price: Option<f64>,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct TransactionDate {
+    year: i32,
+    month: u32,
+    day: u8
+}
+
+impl TransactionDate {
+    fn match_month(&self) -> Month {
+        match self.month {
+            1 => Month::January,
+            2 => Month::February,
+            3 => Month::March,
+            4 => Month::April,
+            5 => Month::May,
+            6 => Month::June,
+            7 => Month::July,
+            8 => Month::August,
+            9 => Month::September,
+            10 => Month::October,
+            11 => Month::November,
+            12 => Month::December,
+            _ => Month::January
+        }
+    }
+}
+    
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
@@ -33,9 +61,21 @@ async fn manual_hello() -> impl Responder {
 
 async fn index(item: web::Json<Portfolio>) -> impl Responder {
     println!("model: {:?}", &item);
-    let start = datetime!(2020-1-1 0:00:00.00 UTC);
-    let end = datetime!(2020-1-31 23:59:59.99 UTC);
-    get_quotes("AAPL", &start, &end).await
+    for n in item.portfolio.iter() {
+        let start = OffsetDateTime::now_utc()
+            .replace_year(n.buy_date.year).unwrap()
+            .replace_month(n.buy_date.match_month()).unwrap()
+            .replace_day(n.buy_date.day).unwrap()
+            .replace_time(time!(0:00:00));
+        let end = OffsetDateTime::now_utc()
+            .replace_year(n.sell_date.as_ref().unwrap().year).unwrap()
+            .replace_month(n.sell_date.as_ref().unwrap().match_month()).unwrap()
+            .replace_day(n.sell_date.as_ref().unwrap().day).unwrap()
+            .replace_time(time!(23:59:59));
+        get_quotes(&n.ticker, &start, &end).await;
+
+    }
+    HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
